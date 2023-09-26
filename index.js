1a1e7923ff7591c119895b114a3d4bf66068c8c0
Shreyas Salankimatt <shreyassalankimatt@gmail.com>
2023-09-26 21:49:10 +0530

    Changed Working

--- index.js
+++ index.js
@@ -1,68 +1,42 @@
 const downloadButton = document.querySelector(".download-paper");
 const helperText = document.querySelector(".helper-text");
 
-const generateURL = (name, number) => {
-    let urlToCheck = `https://vtu.ac.in/pdf/QP/${name}.pdf`;
-    fetch(urlToCheck, { method: "HEAD" })
-        .then((response) => {
-            if (response.status === 200) {
-                return urlToCheck;
-            } else {
-                urlToCheck = `https://vtu.ac.in/pdf/QP/${name}set${number}.pdf`;
-                fetch(urlToCheck, { method: "HEAD" })
-                    .then((response) => {
-                        if (response.status === 200) {
-                            return urlToCheck;
-                        } else {
-                            return new Error("Not existing");
-                        }
-                    })
-            }
-        })
-        .catch((error) => {
-            return new Error("An Error Occoured", error);
-        });
+const generateURL = (name, set1, set2) => {
+  let papersites = [];
+  papersites.push(`https://vtu.ac.in/pdf/QP/${name}.pdf`);
+  if (set1) {
+    papersites.push(`https://vtu.ac.in/pdf/QP/${name}set1.pdf`);
+  }
+  if (set2) {
+    papersites.push(`https://vtu.ac.in/pdf/QP/${name}set2.pdf`);
+  }
+  return papersites;
 };
 
 const downloadPaper = () => {
-    const subjectName = document
-        .querySelector("#subject-code")
-        .value.toUpperCase();
-    const setNumber = document.querySelector("#set").value;
+  const subjectName = document
+    .querySelector("#subject-code")
+    .value.toUpperCase();
 
-    if (!subjectName && !setNumber) {
-        helperText.textContent = "Enter Values";
-        helperText.classList.remove("invisible");
-        return;
-    }
+  if (!subjectName) {
+    helperText.textContent = "Enter Subject Code.";
+    helperText.classList.remove("invisible");
+    return;
+  }
 
-    const url = generateURL(subjectName, setNumber);
+  const set1check = document.getElementById("set-1").checked;
+  const set2check = document.getElementById("set-2").checked;
 
-    if (url instanceof Error) {
-        helperText.textContent = "Paper Not found, perhaps you mistyped the set/subject code?";
-        helperText.classList.remove("invisible");
-        return;
-    }
+  let urls = generateURL(subjectName, set1check, set2check);
 
-    return new Promise(function (resolve, reject) {
-        // Get file name from url.
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = "blob";
-        xhr.onload = function () {
-            resolve(xhr);
-        };
-        xhr.onerror = reject;
-        xhr.open("GET", url);
-        xhr.send();
-    }).then(function (xhr) {
-        const a = document.createElement("a");
-        a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
-        a.download = url.includes("set") ? `subjectName-set-${setNumber}` : "subjectName"; // Set the file name.
-        a.style.display = "none";
-        document.body.appendChild(a);
-        a.click();
-        return xhr;
-    });
+  for (const url of urls) {
+    const a = document.createElement("a");
+    a.href = url;
+    a.target = "_blank"; // Set the file name.
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
 };
 
 downloadButton.addEventListener("click", downloadPaper);
